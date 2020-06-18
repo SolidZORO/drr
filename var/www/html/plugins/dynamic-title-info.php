4fa51b75e01c704022fb9a600abbeff89cf9546e
SolidZORO <solidzoro@live.com>
2020-06-18 20:23:18 +0800

    fix: use PHP native syntax, avoid flashing pages

--- var/www/html/plugins/dynamic-title-info.php
+++ var/www/html/plugins/dynamic-title-info.php
@@ -3,49 +3,104 @@
 /**
  * Dynamic Title Info (Distinguish the current environment, e.g. prod or dev)
  *
+ * Adminer API @link https://www.adminer.org/en/extension/#api
+ *
  * @author SolidZORO, solidzoro@live.com
  */
 class AdminerDynamicTitleInfo
 {
-    function head()
+    private $isLocalhost;
+
+    /**
+     * AdminerDynamicTitleInfo constructor.
+     *
+     * @param null $isLocalhost
+     */
+    function __construct($isLocalhost = null)
     {
-        // for IDE syntax highlighting hacking :/
-        $scriptCode = "<script>
-            window.addEventListener('DOMContentLoaded', () => {
-              const hostname = location.hostname;
-            
-              const htmlElm = document.querySelector('html');
-              const bodyElm = document.querySelector('body');
-              const h1Elm = document.querySelector('#menu #h1');
-              const menuElm = document.querySelector('#menu h1');
-              const versionElm = document.querySelector('#menu .version');
-              
-              // for dev
-              h1Elm.text = hostname;
-              let globalBgColor = '#fff';
-              let textColor = '#e8ff00';
-              let bgColor = '#111';
-            
-              // for prod
-              if (!['localhost', '192.168.', '127.0.'].includes(hostname)) {
-                h1Elm.text = 'production';
-                
-                globalBgColor = '#f9fbe7';
-                textColor = '#fff';
-                bgColor = '#4caf50';
-              }
-              
-              h1Elm.style.color = textColor;
-              menuElm.style.backgroundColor = bgColor;
-            
-              versionElm.style.fontSize = 'xx-small';
-              versionElm.style.color = textColor;
-              htmlElm.style.backgroundColor = globalBgColor;
-              bodyElm.style.backgroundColor = globalBgColor;
-            });
-        </script>";
+        $this->isLocalhost = $isLocalhost;
+    }
 
 
-        echo script(str_replace(['<script>', '</script>'], "", $scriptCode));
+    /**
+     * @return string
+     */
+    private function _getHttpHostName(): string
+    {
+        if (!$_SERVER['HTTP_HOST']) {
+            return 'not-host';
+        }
+
+        $hostArr = explode(':', $_SERVER['HTTP_HOST']);
+        if (!$hostArr) {
+            return 'not-host-arr';
+        }
+
+        return $hostArr[0];
+    }
+
+    /**
+     * @param string[] $whitelist
+     *
+     * @return bool
+     */
+    private function _isLocalhost($whitelist = ['127.0.0.1', 'localhost', '::', '::1', '::ffff:']): bool
+    {
+        $this->isLocalhost = in_array($this->_getHttpHostName(), $whitelist);
+
+        return $this->isLocalhost;
+    }
+
+    /**
+     * @return string
+     */
+    function name(): string
+    {
+        $titleName = $this->_isLocalhost() ? $this->_getHttpHostName() : 'production';
+
+        return "<span id='h1'>{$titleName}</span>";
+    }
+
+    /**
+     * @return void
+     */
+    function head(): void
+    {
+        // for prod
+        $prodStyle = [
+            '--global-bg-color' => '#f9fbe7',
+            '--text-color'      => '#fff',
+            '--bg-color'        => '#4caf50',
+        ];
+
+        // for dev
+        $devStyle = [
+            '--global-bg-color' => '#fff',
+            '--text-color'      => '#e8ff00',
+            '--bg-color'        => '#111',
+        ];
+
+        $style = $this->isLocalhost ? $devStyle : $prodStyle;
+
+        echo "<style>
+          html, body {
+            background-color: {$style['--global-bg-color']};
+          }
+
+          #menu .version {
+            font-size: 10px;
+          }
+          
+          #menu h1 {
+            background-color: {$style['--bg-color']};
+            transition: all 0.3s;
+          }
+          
+          #menu #h1, 
+          #menu .version {
+            color: {$style['--text-color']};
+            transition: all 0.3s;
+          }
+        </style>";
     }
 }
