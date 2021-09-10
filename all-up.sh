a05da59a0aca65e72e133792b4cab73a337399ef
Jason Feng <solidzoro@live.com>
2021-09-10 13:14:32 +0800

    feat: split adminer yml

--- all-up.sh
+++ all-up.sh
@@ -4,6 +4,7 @@ cd "$(dirname "$0")" || exit
 
 docker-compose -f docker-compose.nginx.yml up -d && printf "\n\n✴️  Nginx is Up!\n\n"
 docker-compose -f docker-compose.mysql.yml up -d && printf "\n\n✴️  MySQL is Up!\n\n"
+docker-compose -f docker-compose.adminer.yml up -d && printf "\n\n✴️  Adminer is Up!\n\n"
 #docker-compose -f docker-compose.acme.yml up -d && printf "\n\n✴️  Acme is Up!\n\n"
 
 docker container ls && printf "\n\n"
